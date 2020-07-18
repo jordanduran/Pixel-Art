3678ca783779aa250bc9686394f0d1356c90ab5a
Jordan Duran <jordanduran@static-192-165-1-12.cust.crystone.se>
2020-07-18 19:37:26 -0400

    ColorPicker component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Canvas from './components/Canvas';
+import ColorPicker from './components/ColorPicker';
 
 const App = () => {
   return (
     <div className='App'>
+      <ColorPicker />
       <Canvas />
     </div>
   );
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pixel from './Pixel';
+import Colors from './Colors';
+
+const ColorPicker = props => {
+  return (
+    <div className='colorpicker'>
+      {Colors.map((color, index) => {
+        return <Pixel key={index} background={color} />;
+      })}
+    </div>
+  );
+};
+
+export default ColorPicker;
