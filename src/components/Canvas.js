f06f1441c77908d35ed4e63e34994ae3540bdcc1
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-07-18 19:30:21 -0400

    cleanup, Pixel and Canvas component

---
+++ src/components/Canvas.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Colors from './Colors';
+import Pixel from './Pixel';
+
+const Canvas = () => {
+  const [matrix, setMatrix] = useState(
+    Array(30)
+      .fill()
+      .map(() => Array(30).fill(0))
+  );
+  return (
+    <div className={'canvas'}>
+      {matrix.map((row, rowIndex) =>
+        row.map((_, colIndex) => {
+          return (
+            <Pixel
+              key={`${rowIndex}-${colIndex}`}
+              background={Colors[matrix[rowIndex[colIndex]]]}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Canvas;
---
+++ src/components/Pixel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pixel = props => {
+  return <div className={`${props.background} pixel`}></div>;
+};
+
+export default Pixel;
